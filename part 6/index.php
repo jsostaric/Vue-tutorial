30f063398543dda6cda9237ace97be1a023c7d92
Jurica Šoštarić <bugificiran@gmail.com>
2019-03-04 14:48:07 +0100

    Tutorial for Event handler in Vue.js

--- part 6/index.php
+++ part 6/index.php
@@ -12,8 +12,16 @@
   </head>
   <body>
   <div id="app">
+    <h1>Count: {{count}}<h1>
+    <button @click="countUp" class="btn btn-primary">Count Up</button>
+    <button @click="countDown" class="btn btn-primary">Count Down</button>
+    <hr>
     <h1>{{message}}<h1>
-    <input type="text" v-model="message" />
+    <p>
+      Visit: <a :href="url">{{cleanUrl}}</a> 
+    </p>
+    <input type="text" class="form-control" v-model="url" />
+    <button @click="humanizeUrl" class="btn btn-primary">Humanize Me!</button>
 </div>
 
    
@@ -24,8 +32,23 @@ var app = new Vue({
     el: "#app",
     data: {
         message: "This is new message",
+        count: 0,
+        url: "",
+        cleanUrl: ""
+    },
 
+    methods: {
+      countUp: function(){
+        this.count += 1
+      },
+      countDown: function() {
+        this.count -= 1
+      },
+      humanizeUrl: function() {
+        this.cleanUrl = this.url.replace(/^https?:\/\//, "").replace(/\/$/, "")
+      }
     }
+
 
 })
 
