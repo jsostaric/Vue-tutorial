222b133259c55c048d2f615a3f93b2ccd0533595
Jurica Šoštarić <bugificiran@gmail.com>
2019-03-04 14:02:48 +0100

    first commit

---
+++ part 1/readme.md
@@ -0,0 +1,2 @@
+Vue.js tutorial for beginners
+- learning Vue from scratch
---
+++ part 2/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Vue.js Tutorials</title>
+  </head>
+  <body>
+  <div id="app">
+    <h1 v-text="message">Hello<h1>
+</div>
+
+   
+    <!-- development version, includes helpful console warnings -->
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+<script>
+var app = new Vue({
+    el: "#app",
+    data: {
+        message: "This is new message"
+    }
+
+})
+
+</script>
+
+  </body>
+</html>
