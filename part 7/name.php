b059df153cec02fb21eee793fd0bfa8180842f20
Jurica Šoštarić <bugificiran@gmail.com>
2019-03-04 15:18:14 +0100

    two tasks for practice

---
+++ part 7/name.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Vue.js Tutorials</title>
+  </head>
+  <body>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h1>Hello {{fullname}}</h1>
+        <ul class="text-left">
+          <li>First Name: {{first}}</li>
+          <li>Last Name: {{last}}</li>
+        </ul>
+        <hr>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group">
+          <label>First Name:</label>
+          <input type="text" class="form-control" v-model="first" />
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="form-group">
+          <label>Last name:</label>
+          <input type="text" class="form-control" v-model="last" />
+        </div>
+      </div>
+    </div>
+</div>
+
+   
+    <!-- development version, includes helpful console warnings -->
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+<script>
+var app = new Vue({
+    el: "#app",
+    data: {
+        message: "This is new message",
+        first: "",
+        last: ""
+    },
+
+    computed: {
+      fullname: function() {
+        return this.first + " " + this.last
+      }
+    }
+
+})
+
+</script>
+
+  </body>
+</html>
